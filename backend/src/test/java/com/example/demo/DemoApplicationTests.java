26ac7438f8517955206f154db0662e152504ab38
NADIR Salma <135320754+salmanadir@users.noreply.github.com>
2025-09-11 17:04:49 +0100

    Update DemoApplicationTests.java

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+
 
 @SpringBootTest
 @Disabled("Disabled: requires Docker env to run")
